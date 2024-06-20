124b44f752c59a9147d52266c618319b9f632d4d
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-06-20 15:54:16 +0200

    File parsing eneabled

---
+++ gcsnap/targets.py
@@ -0,0 +1,111 @@
+import os
+from typing import Union
+
+from gcsnap.configuration import Configuration
+from gcsnap.rich_console import RichConsole 
+
+class Target():
+    def __init__(self, configuration: Configuration):
+        self.arguments = configuration.arguments
+        self.targets = configuration.targets
+        self.console = RichConsole()
+
+        # empty dictionary to store the targets
+        self.targets_lists = {}
+
+    def run(self) -> None:
+        self.parse_targets()
+
+    def parse_targets(self, targets: Union[str, list] = None) -> dict:
+        if targets is None:
+            targets = self.targets
+
+        for target in targets:
+            if os.path.isfile(target):
+                if target.endswith('.clans'):
+                    tmp = self.get_clusters_from_clans(target)
+                    # TODO: Not clear if there can be muliple .clans files, doesn't seem in gcsnap1 as tmp would be returned
+                else:
+                    self.get_targets_from_file(target)
+            else:
+                # TODO: The default value for label if not from file name is 'default'??
+                label = self.arguments['out_label']['value']
+                if label not in self.targets_lists:
+                    self.targets_lists[label] = []
+                
+                self.targets_lists[label].append(target)
+
+    def get_targets_from_file(self, target_file: str) -> None:
+        curr_label = target_file.split('/')[-1].split('.')[0]
+        if curr_label not in self.targets_lists:
+            self.targets_lists[curr_label] = []
+
+        is_fasta = False
+        with open(target_file, 'r') as f:
+            content = f.readlines()
+
+        for line in content:
+            if line.startswith('>'):
+                is_fasta = True
+                curr_target = line.split(' ')[0].split(':')[0].split('|')[0].split('_#')[0].replace('>','').strip()
+
+            elif not is_fasta:
+                curr_target = line.strip().split()[0]
+
+            if curr_target not in self.targets_lists[curr_label]:
+                self.targets_lists[curr_label].append(curr_target)        
+
+
+    def get_clusters_from_clans(self, clans_file: str) -> dict:
+        ncbis_ordered = self.get_ncbicodes_order_in_clans(clans_file)
+        cluster_codes = self.arguments['cluster_patterns']['value']
+
+        clusters = {}
+        
+        if cluster_codes != None:
+            found_seqgroup = False
+            with open(clans_file, 'r') as f:
+                content = f.readlines()
+
+            for line in content:
+                for cluster_code in cluster_codes:
+                    if '<seqgroup' in line:
+                        found_seqgroup = True
+                        found_allowed_cluster = False
+
+                    elif found_seqgroup:
+                        if 'name=' in line and cluster_code in line:
+                            current_cluster = line.split('=')[-1].strip()
+                            found_allowed_cluster = True
+                        
+                        elif 'numbers=' in line and found_allowed_cluster:
+                            numbers = line.split('=')[-1].split(';')[:-1]
+                            numbers = [int(i) for i in numbers]
+                            numbers = [ncbis_ordered[i] for i in numbers]
+                            clusters[current_cluster] = numbers
+
+                            found_allowed_cluster = False
+
+        else:
+            label = clans_file.split('/')[-1].split('.')[0]
+            clusters[label] = ncbis_ordered
+
+        return clusters
+    
+    def get_ncbicodes_order_in_clans(self, clans_file: str) -> list:   
+        ncbids_ordered = []
+
+        found_seq = False
+        with open(clans_file, 'r') as clans:
+            for line in clans:
+                if '<seq>' in line:
+                    found_seq = True
+                elif '</seq>' in line:
+                    found_seq = False
+                elif found_seq and line.startswith('>'):
+                    line = line[1:]
+                    ncbi_code = line.split(' ')[0].split(':')[0].split('|')[0].split('_#')[0].replace('>','').strip()
+                    ncbids_ordered.append(ncbi_code)
+                    
+        return ncbids_ordered
+ 
