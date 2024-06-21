eccb0d2b8f6087def5e4300f1e9079cee42b833b
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-06-22 00:02:29 +0200

    Added making results unique functionality and logging

--- gcsnap/sequence_mapping_online.py
+++ gcsnap/sequence_mapping_online.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import numpy as np
 
 from gcsnap.configuration import Configuration
-from gcsnap.rich_console import RichConsole 
 from gcsnap.utils import processpool_wrapper
 from gcsnap.uniprot_dbs_dict import UniprotDict
 
@@ -10,17 +10,17 @@ from gcsnap.uniprot_api import check_id_mapping_results_ready
 from gcsnap.uniprot_api import get_id_mapping_results_link
 from gcsnap.uniprot_api import get_id_mapping_results_search
 
+import logging
+logger = logging.getLogger(__name__) # inherits configuration from main logger
+
 
 class SequenceMappingOnline:
     def __init__(self, config: Configuration, target_list: list, to_type: str):
-        self.config = config
-        self.console = RichConsole()
-
         self.target_list = target_list
         self.to_type = to_type
 
         # extract needed values from config
-        self.cores = self.config.arguments['n_cpu']['value']
+        self.cores = config.arguments['n_cpu']['value']
 
     def run(self):
         self.create_uniprot_dbs_with_targets()
@@ -28,17 +28,23 @@ class SequenceMappingOnline:
         mapping_df_list = processpool_wrapper(self.cores, parallel_args, self.do_mapping)
         # concatenate all mapping dfs to one
         self.mapping_df = pd.concat(mapping_df_list, ignore_index=True)
-        # extract all results for self.to_type from that df
-        self.create_result_list()
+        # check and error logging is done here. 
+        # The reason is that logging from several processes is not that easy
+        # https://docs.python.org/3/howto/logging-cookbook.html#logging-to-a-single-file-from-multiple-processes
+        self.log_failed()
+
+    def get_codes(self, id_type: str = None) -> list:
+        if id_type is None:
+            id_type = self.to_type
+        
+        return self.mapping_df[id_type].tolist()
            
     def create_uniprot_dbs_with_targets(self) -> None:
         uniprot_dict = UniprotDict()
+        # fill uniprot_dbs with the targets        
         uniprot_dict.assign_target_to_type(self.target_list)
-        # fill uniprot_dbs with the targets
+        self.supported = uniprot_dict.supported
         self.uniprot_dict = uniprot_dict.uniprot_dbs
-
-    def create_result_list(self):
-        self.result_list = self.mapping_df[self.to_type].tolist()
 
     def create_parallel_input(self, api_limit: int = 10000) -> list[tuple]:
         parallel_input_args = []
@@ -88,8 +94,9 @@ class SequenceMappingOnline:
         mapping_api_results = self.run_mapping_api(ids, from_db, to_db)
         mapping_extracted = self.extract_mapping_from_api_results(mapping_api_results, from_type, to_type)
         mapping_df = self.create_mapping_df(mapping_extracted)
+        unique_mapping_df = self.make_unique(mapping_df, from_type, to_type)
         
-        return mapping_df
+        return unique_mapping_df
 
     def run_mapping_api(self, ids: list, from_db: str, to_db: str) -> list:  
         #ids, from_db, to_db, from_type, to_type = arg
@@ -105,7 +112,7 @@ class SequenceMappingOnline:
         else:
             return None
 
-    def extract_mapping_from_api_results(self, results: list, from_type:str , to_type:str) -> dict:
+    def extract_mapping_from_api_results(self, results: list, from_type: str , to_type: str) -> dict:
         # the UniProt Rest API returns different formats depending on what db is searched
         # for instance when mapping to EMBL-Genbank, the results is a list with 1 level dict:
             # [{'from': 'A0A0E3MFP2', 'to': 'AKA75089.1'},
@@ -120,15 +127,16 @@ class SequenceMappingOnline:
                 
         for result in results:
             extracted['from_id'].append(result['from'])
+            # get result_id
             if isinstance(results[0]['to'], dict):
                 extracted['to_id'].append(result['to']['primaryAccession'])
             else:
                 extracted['to_id'].append(result['to'])
             
         return extracted    
-    
+        
     def create_mapping_df(self, mapping_dict: dict) -> pd.DataFrame:        
-        id_types = ['UniProtKB-AC','UniProtKB-ID','RefSeq','GeneID','Ensembl','UniParc','EMBL-CDS']
+        id_types =  self.supported
         mappings = {id_type: [] for id_type in id_types}
         
         from_type = mapping_dict['from_type']
@@ -138,20 +146,62 @@ class SequenceMappingOnline:
         
         # fill remaining cols with nan
         for id_type in list(set(id_types).difference(set([from_type,to_type]))):
-            mappings[id_type] += ['nan'] * len(mapping_dict['from_id'])
+            mappings[id_type] += [np.nan] * len(mapping_dict['from_id'])
                 
         return pd.DataFrame.from_dict(mappings)
-    
-    def merge_mapping_dfs(self, mapping_df: pd.DataFrame, columns_to_merge: list = ['EMBL-CDS']) -> pd.DataFrame:
+
+    def make_unique(self, mapping_df: pd.DataFrame, from_type: str, to_type: str) -> pd.DataFrame:
+        # make copy before handling data frame
+        df = mapping_df.copy()
+
+        # add length colum for sorting
+        # Rule of thumb, just take the first one for UniParc.            
+        # The longer UniProt-KB are currated automatically. Take the samller one
+        df['Length'] = df[to_type].apply(len) 
+        # sort, default ascending
+        df = df.sort_values(by='Length')             
+        
+        # is duplicated detects Nan as duplicated, hence most rows are dropped for GeneID
+        mask = df[from_type].notna()
+
+        # Checking for duplicates only on non-NaN values while preserving the original index
+        is_duplicated = df.loc[mask, from_type].duplicated(keep=False).reindex(df.index, fill_value=False)
+
+        # DataFrame of non-unique entries, selecting the first occurrence of each duplicated entry
+        # sorting is not changed by group by
+        first_non_unique_df = df[is_duplicated].groupby(from_type, sort=False).head(1)
+        # DataFrame of unique entries (i.e., entries that are not duplicated)
+        unique_df = df[~is_duplicated]
+        # Combine the two DataFrames
+        filtered_unique_df = pd.concat([unique_df, first_non_unique_df], ignore_index=True)
+        
+        # drop sorting column
+        filtered_unique_df.drop(columns='Length', inplace=True)   
+        return filtered_unique_df
+
+    def log_failed(self) -> None:
+        df = self.mapping_df.copy()
+        # Combine all non-NaN entries from the specified columns into one list
+        non_nan_entries = [df[col].dropna().tolist() for col in
+                            list(set(self.supported).difference(set([self.to_type])))]
+        # Flatten the list of lists
+        non_nan_entries = [entry for sublist in non_nan_entries for entry in sublist]
+        # Create a set of all non-NaN entries
+        non_nan_entries = set(non_nan_entries)
+
+        for id in list(set(self.target_list).difference(non_nan_entries)):
+            logger.warning(f'Failed to map id {id} to {self.to_type}')    
+
+    def merge_mapping_dfs(self, mapping_df: pd.DataFrame, key_column: str = 'UniProtKB-AC',
+                          columns_to_merge: list = ['EMBL-CDS']) -> pd.DataFrame:
         # Merge DataFrames on 'UniProtKB-AC'
-        merged_df = pd.merge(self.mapping_df, mapping_df[['UniProtKB-AC'] + columns_to_merge], 
-                             on='UniProtKB-AC', how='left', suffixes=('', '_y'))
+        merged_df = pd.merge(self.mapping_df, mapping_df[[key_column] + columns_to_merge], 
+                             on=key_column, how='left', suffixes=('_y', ''))
         # Update 'EMBL-CDS' in df1 with values from df2
         for column in columns_to_merge:
-            merged_df[column] = merged_df[column].combine_first(merged_df[f'{column}_y'])
             # Drop the additional 'EMBL-CDS_y' column
             merged_df.drop(columns=[f'{column}_y'], inplace=True)
-        self.mapping_df = merged_df.copy()
+        self.mapping_df = merged_df.copy()                    
 
 
 
