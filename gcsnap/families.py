cf1da4e7e615dbd605622357907a21b95bddbb0d
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-07-07 17:57:55 +0200

    Functionality to find families within flanking genes using MMseqs

---
+++ gcsnap/families.py
@@ -0,0 +1,105 @@
+import os
+
+from gcsnap.configuration import Configuration
+from gcsnap.rich_console import RichConsole
+from gcsnap.genomic_context import GenomicContext
+from gcsnap.mmseqs_cluster import MMseqsCluster
+
+from gcsnap.utils import processpool_wrapper
+from gcsnap.utils import split_dict_chunks
+
+class Families:
+    def __init__(self, config: Configuration, gc: GenomicContext, out_label: str):
+        self.config = config
+        self.cores = config.arguments['n_cpu']['value']
+
+        # set arguments
+        self.out_label = out_label
+        self.out_dir = os.path.join(os.getcwd(), f'{out_label}_all_against_all_searches')
+        self.make_dir(self.out_dir)        
+        self.gc = gc
+        self.syntenies = gc.get_syntenies()
+
+        self.console = RichConsole()
+
+    def get_families(self) -> dict:
+        return self.families_adapted
+
+    def run(self) -> None:
+        self.find_cluster()
+
+        with self.console.status('Assigning families to flanking genes'):
+
+            # 1. Add the family to the flanking genes
+            # do in parallel, however, each needs the self.cluster_list and self.cluster_order
+            # is this might be large, we use as many batches as there are cores
+            parallel_args = split_dict_chunks(self.syntenies, self.cores)  
+            # a list of tuple[dict, list] is returned
+            result_list = processpool_wrapper(self.cores, parallel_args, self.assign_families)
+            # combine results
+            self.families = {k: v for tup in result_list for k, v in tup[0].items()}
+            curr_numbers = [num for tup in result_list for num in tup[1]]
+            # sort the curr_numbers
+            curr_numbers = sorted(list(set(curr_numbers)))
+
+            # 2. adapt the families where its outside possible ranges
+            parallel_args = [(sub_dict, curr_numbers) 
+                             for sub_dict in split_dict_chunks(self.families, self.cores)  ] 
+            dict_list = processpool_wrapper(self.cores, parallel_args, self.adapt_families)
+            # combine results
+            self.families_adapted = {k: v for dict_ in dict_list for k, v in dict_.items()}  
+
+    def find_cluster(self) -> list:
+        # call MMseqsCluster
+        cluster = MMseqsCluster(self.config, self.gc, self.out_dir)
+        cluster.run()
+        self.cluster_list = cluster.get_clusters_list() 
+        self.cluster_order = cluster.get_cluster_order()           
+        
+    def assign_families(self, args: tuple) -> tuple[dict,list]:
+        syntenies = args
+        # loop over all targets in the chunk
+        curr_numbers = []
+        for k in syntenies.keys():
+            syntenies[k]['flanking_genes']['family'] = []
+            for i, ncbi_code in enumerate(syntenies[k]['flanking_genes']['ncbi_codes']):
+                protein_name = syntenies[k]['flanking_genes']['names'][i]
+                try:
+                    protein_family = self.cluster_list[self.cluster_order.index(ncbi_code)]
+                except:
+                    protein_family = 10000
+
+                if protein_name == 'pseudogene':
+                    protein_family = 10000
+                if ncbi_code == k:
+                    protein_family = max(self.cluster_list)+1
+
+                syntenies[k]['flanking_genes']['families'].append(protein_family)
+                curr_numbers.append(protein_family)
+
+                if ncbi_code == syntenies[k]['assembly_id'][0]:
+                    syntenies[k]['target_family'] = protein_family 
+
+        return (syntenies , curr_numbers) 
+    
+    def adapt_families(self, args: tuple) -> dict:
+        # TODO: Check what this curr_numbers is
+        syntenies, curr_numbers = args
+        for k in syntenies.keys():
+            for i, _ in enumerate(syntenies[k]['flanking_genes']['ncbi_codes']):
+                protein_family = syntenies[k]['flanking_genes']['families'][i]
+
+                if protein_family <= max(self.cluster_list):
+                    cluster_range = range(min(self.cluster_list), max(self.cluster_list)+1)
+                    if protein_family != cluster_range[curr_numbers.index(protein_family)]:
+                        protein_family = cluster_range[curr_numbers.index(protein_family)]
+                        syntenies[k]['flanking_genes']['families'][i] = protein_family
+
+                syntenies[k]['target_family'] = syntenies[k]['flanking_genes']['families'][
+                    syntenies[k]['flanking_genes']['ncbi_codes'].index(syntenies[k]['assembly_id'][0])]
+
+        return syntenies
+    
+    def make_dir(self, path: str) -> None:
+        if not os.path.isdir(path):
+            os.mkdir(path)
---
+++ gcsnap/mmseqs_cluster.py
@@ -0,0 +1,121 @@
+import os
+import subprocess
+import numpy as np
+# pip install scipy
+from scipy.cluster import hierarchy
+from scipy.spatial import distance
+
+from gcsnap.configuration import Configuration
+from gcsnap.genomic_context import GenomicContext
+from gcsnap.rich_console import RichConsole
+
+class MMseqsCluster:
+    def __init__(self, config: Configuration, gc: GenomicContext, out_dir: str):
+        self.config = config
+        self.cores = config.arguments['n_cpu']['value']
+        self.max_evalue = config.arguments['max_evalue']['value']
+        self.min_coverage = config.arguments['min_coverage']['value']
+        self.num_iterations = config.arguments['num_iterations']['value']
+        self.mmseqs_executable = r'{}'.format(config.arguments['mmseqs_executable_path']['value'])
+        self.default_base = config.arguments['default_base']['value']
+
+        # set arguments
+        self.gc = gc
+        self.out_dir = out_dir
+
+        # TODO: Sensitivity is hard coded?   
+        self.sensitivity = 7.5
+
+        self.console = RichConsole()
+
+    def run(self) -> None:
+        with self.console.status('Prepare data for MMseqs'):
+            self.fasta_file = self.gc.write_to_fasta('flanking_sequences.fasta', 
+                                            self.out_dir, exclude_pseudogenes = False)  
+            self.cluster_order = self.gc.get_fasta_order(False) 
+        with self.console.status('Running MMseqs'):            
+            self.run_mmseqs()
+        with self.console.status('Extracting distance matrix'):
+            self.extract_distance_matrix()
+        with self.console.status('Find clusters'):
+            self.find_clusters()
+            self.mask_singleton_clusters()        
+
+    def get_distance_matrix(self) -> np.array:
+        return self.distance_matrix
+
+    def get_clusters_list(self) -> list[int]:
+        return self.cluster_list      
+
+    def get_cluster_order(self) -> list[str]:
+        return self.cluster_order       
+
+    def run_mmseqs(self) -> None:
+            self.mmseqs_results = os.path.join(self.out_dir, '{}_{}.mmseqs'.format(
+                os.path.basename(self.fasta_file)[:-6], self.max_evalue))
+            
+            # TODO: Why checking if file exists first?
+            if not os.path.isfile(self.mmseqs_results):
+                try:
+                    _, stderr = self.mmseqs_command('mmseqs')
+                    if len(stderr) > 0:
+                        raise FileNotFoundError
+                except FileNotFoundError:
+                    try:
+                        _, stderr = self.mmseqs_command(self.mmseqs_executable)
+                    except:
+                        self.console.print_error('No MMseqs installation was found.') 
+                        self.console.print_hint('Please install MMseqs or add the path to the executable to config.yaml.')
+                        exit(1)                    
+
+    def mmseqs_command(self, mmseqs :str) -> tuple:
+        # returns stdout,stderr
+        command = [mmseqs, 
+                'easy-search', 
+                self.fasta_file, 
+                self.fasta_file, 
+                self.mmseqs_results, 
+                self.out_dir, 
+                '-e', str(self.max_evalue), 
+                '-s', str(self.sensitivity),
+                '-c', str(self.min_coverage),
+                '--num-iterations', str(self.num_iterations),
+                '--threads', str(self.cores),
+                '--format-output',
+                'query,target,evalue']
+        
+        result = subprocess.run(command, capture_output=True, text=True, shell=True)        
+        return result.stdout, result.stderr       
+    
+    def extract_distance_matrix(self) -> None:
+        # crate base distance matrix
+        distance_matrix = [[self.default_base if i!=j else 0 for i in self.cluster_order] 
+                        for j in self.cluster_order]
+        queries_labels = {query: i for i, query in enumerate(self.cluster_order)}
+
+        # read mmseqs results
+        with open(self.mmseqs_results, 'r') as f:
+            mmseqs_records = f.readlines()
+
+        for hsp in mmseqs_records:
+            hsp = hsp.split()
+            if len(hsp) > 0:
+                query = hsp[0].split('|')[0]
+                query_index = queries_labels[query]
+                target = hsp[1].split('|')[0]
+                if target != query:
+                    target_index = queries_labels[target]
+                    distance_matrix[query_index][target_index] = 0
+                    distance_matrix[target_index][query_index] = 0
+
+        self.distance_matrix = np.array(distance_matrix)      
+
+    def find_clusters(self, t: int = 0) -> None:
+        distance_matrix = distance.squareform(self.distance_matrix)
+        linkage = hierarchy.linkage(distance_matrix, method = 'single')
+        clusters = hierarchy.fcluster(linkage, t, criterion = 'distance')
+        self.cluster_list = [int(i) for i in clusters]
+
+    def mask_singleton_clusters(self, mask: int = 0) -> None:
+        self.cluster_list = [mask if list(self.cluster_list).count(value) == 1 
+                             else value for value in self.cluster_list]      
