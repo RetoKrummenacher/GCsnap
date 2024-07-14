f448ff33ca7119a9a89be0affb433f7448d2e2fc
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-07-14 18:12:47 +0200

    added proper exception printing to debug

--- gcsnap/GCsnap.py
+++ gcsnap/GCsnap.py
@@ -201,7 +201,8 @@ def find_ncbi_code_assembly(ncbi_code, database_assembly_mapping):
 
 							assembly_link = database_assembly_mapping[assembly_id]
 		t2.stop()
-	except:
+	except Exception as e:
+		print(e)
 		assembly_id = 'nan'
 		assembly_source = 'nan'
 		assembly_link = 'nan'
