158bfa33b4226592beef4ed409b92f1e44ef7d45
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-07-22 11:24:40 +0200

    Added all imports for the Package and docstring to __init__.py

--- gcsnap/__init__.py
+++ gcsnap/__init__.py
@@ -1,26 +1,68 @@
 """
-Gcsnap.py
+GCsnap Package
+
+This package contains the main classes and functions to run the GCsnap pipeline.
+The following modules are included:
+
+Modules used in the pipeline:
+    - rich_console: RichConsole class to print messages in color and style.
+    - configuration: Configuration class to parse the configuration file and arguments.
+    - timing: Timing and Timer class to measure the time of each step.
+    - targets: Target class to parse the target list.
+    - sequence_mapping: SequenceMapping class to map sequences to the genome.
+    - assemblies: Assemblies class to finde, download and parse the assemblies.
+    - genomic_context: GenomicContext class to store the genomic context.
+    - sequences: Sequences class to find and extract sequences.
+    - families: Families class to find and assign families to genes.
+    - families_functions_structures: FamiliesFunctionsStructures class to find and
+        assign functions and structures to families.
+    - operons: Operons class to find operons to genes.
+    - taxonomy: Taxonomy class to find and add taxonomy information.
+    - tm_segments: TMsegments class to find and add transmembrane segments.
+    - figures: Figures class to create figures.
+
+Helper modules than are usable standalone as well:
+    - utils: Helper functions to split lists and dictionaries and parallelize functions.
+    - entrez_query: EntrezQuery class to query the NCBI Entrez database.
+    - mmseqs_cluster: MMseqsCluster class to cluster sequences with MMseqs2.
+    - uniprot_api: UniprotAPI class to query the Uniprot API.
+    - uniprot_dbs_dict: UniprotDBsDict class to store the Uniprot databases.
+    - apis: SwissProtAPI, AlphaFoldAPI and UniProtAPI classes to query functional annotation.
+    - assembly_links: AssemblyLinks class to find and store assembly links.
+
 """
 
+# Import classes and functions from various modules of the pipeline in __main__.py
+from .rich_console import RichConsole 
+from .configuration import Configuration 
+from .timing import Timing
+from .targets import Target 
+from .sequence_mapping import SequenceMapping
+from .assemblies import Assemblies
+from .genomic_context import GenomicContext
+from .sequences import Sequences
+from .families import Families
+from .families_functions_structures import FamiliesFunctionsStructures
+from .operons import Operons
+from .taxonomy import Taxonomy
+from .tm_segments import TMsegments
+from .figures import Figures
 
-# import commands for all modules
-# __all__ = ["file1", "file2", "file3"]
+# import helper classes and function
+from .utils import processpool_wrapper, split_dict_chunks, split_list_chunks
+from .entrez_query import EntrezQuery
+from .mmseqs_cluster import MMseqsCluster
+from .uniprot_api import UniprotAPI
+from .uniprot_dbs_dict import UniprotDBsDict
+from .apis import SwissProtAPI, AlphaFoldAPI, UniProtAPI
+from .assembly_links import AssemblyLinks
 
 
-
-
-# The project.toml file you're seeing is a configuration file for a Python project. It's a newer standard that aims to replace setup.py, requirements.txt, MANIFEST.in, and other build-related files. It's part of PEP 621 and PEP 518.
-
-# When you run python setup.py install, Python's setuptools will look for a setup.py file by default. If it doesn't find one, it will look for a pyproject.toml file (not project.toml). If it finds a pyproject.toml file, it will use the build system specified in that file to build and install the project.
-
-# In your case, the build system is specified as setuptools.build_meta, which is a backend that supports PEP 621 and PEP 518. This means that setuptools will use the information in the project.toml file to build and install the project.
-
-# The [project.scripts] section in the project.toml file specifies that the GCsnap command should run the main function in the gcsnap.GCsnap module. This is equivalent to the entry_points argument in a setup.py file.
-
-# The [tool.setuptools] section specifies that the package includes the gcsnap module. This is equivalent to the packages argument in a setup.py file.
-
-# The [project.dependencies] section specifies the dependencies of the project. This is equivalent to the install_requires argument in a setup.py file.
-
-# So, when you run python setup.py install, setuptools will use the information in the project.toml file to install the gcsnap module, its dependencies, and the GCsnap command.
-
-# Please note that the project.toml file should be named pyproject.toml according to PEP 518. If it's not, you might need to rename it.
+# using from GCsnap import *, only the names included in __all__ will be imported.
+__all__ = [
+    'RichConsole', 'Configuration', 'Timing', 'Target', 'SequenceMapping', 'Assemblies',
+    'GenomicContext', 'Sequences', 'Families', 'FamiliesFunctionsStructures',
+    'Operons', 'Taxonomy', 'TMsegments', 'Figures', 'processpool_wrapper', 'split_dict_chunks',
+    'split_list_chunks', 'EntrezQuery', 'MMseqsCluster', 'UniprotAPI', 'UniprotDBsDict',
+    'SwissProtAPI', 'AlphaFoldAPI', 'UniProtAPI', 'AssemblyLinks'
+]
