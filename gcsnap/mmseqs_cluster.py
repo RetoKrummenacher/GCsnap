be35aca2751044d625bb75d63276a8437df33118
RetoKrummenacher <71382164+RetoKrummenacher@users.noreply.github.com>
2024-07-24 15:06:04 +0200

    Improved error message when mmseqs did not run

--- gcsnap/mmseqs_cluster.py
+++ gcsnap/mmseqs_cluster.py
@@ -136,7 +136,7 @@ class MMseqsCluster:
                     self.console.print_hint('Try to clean the temporary folder (e.g., rm -rf /tmp/* on Linux) and run again.') 
                     exit(1)
                 except:
-                    self.console.print_error('No MMseqs installation was found.') 
+                    self.console.print_error('No MMseqs installation was found') 
                     self.console.print_hint('Please install MMseqs or add the path to the executable to config.yaml.')
                     exit(1)                    
 
